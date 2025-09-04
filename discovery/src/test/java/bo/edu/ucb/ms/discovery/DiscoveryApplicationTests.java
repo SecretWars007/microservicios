68302d87543e58ba7d2c9fab8855319b42380e37
SecretWars007 <enriquezariasedwingustavo@gmail.com>
2025-09-03 20:45:58 -0400

    servicio discovery - eureka server

---
+++ discovery/src/test/java/bo/edu/ucb/ms/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ms.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
